96661ba18fb3ee3008231b4409bbb44044bc5a39
nekofar <nekofar@69b2c916-071b-0410-9070-5716e656dd17>
2006-08-15 05:22:54 +0000

    New Import

---
+++ trunk/plugins/Socializer/Socializer.pl
@@ -0,0 +1,33 @@
+# Socializer plugin for Movable Type
+# Author: Milad Nekofar (http://www.nekofar.com)
+# Released under the Artistic License
+#
+# $Id: Socializer.pl,v 1.3 2006/07/16 05:45:42 nekofar Exp $
+
+package MT::Plugin::Socializer;
+
+use strict;
+use MT::Plugin;
+
+my $plugin = MT::Plugin->new ({
+	name => '<MT_TRANS phrase="Socializer">',
+	version => '0.1',
+	description => '<MT_TRANS phrase="Add socializer link to your entries.">',
+	author_name => '<MT_TRANS phrase="Milad Nekofar">',
+	author_link => 'http://weblog.nekofar.com/',
+	l10n_class => 'Socializer::L10N',
+});
+MT->add_plugin($plugin);
+
+use MT::Template::Context;
+MT::Template::Context->add_tag ('EntrySocializerLink',\&socializer_link);
+
+# add socializer link
+sub socializer_link {
+        my($ctx, $arg) = @_;
+        my $entry = $ctx->stash('entry');
+        my $perma_link = $entry->permalink;
+        my $title = $entry->title;
+        my $socializer_link = "http://ekstreme.com/socializer/?url=".$perma_link."&title=".$title."";
+        return $socializer_link;
+}
---
+++ trunk/plugins/Socializer/lib/Socializer/L10N/en_us.pm
@@ -0,0 +1,15 @@
+# Socializer plugin for Movable Type
+# Author: Milad Nekofar (http://www.nekofar.com)
+# Released under the Artistic License
+#
+# $Id: en_us.pm,v 1.1 2006/07/16 05:45:42 nekofar Exp $
+
+package Socializer::L10N::en_us;
+
+use strict;
+
+use base 'Socializer::L10N';
+use vars qw( %Lexicon );
+%Lexicon = ();
+
+1;
