fe83e8d4c2c5646fbadc59653fbee7a218f678ff
nekofar <nekofar@69b2c916-071b-0410-9070-5716e656dd17>
2006-08-15 05:22:54 +0000

    New Import

---
+++ plugins/Socializer/lib/Socializer/L10N.pm
@@ -0,0 +1,11 @@
+# Socializer plugin for Movable Type
+# Author: Milad Nekofar (http://www.nekofar.com)
+# Released under the Artistic License
+#
+# $Id: L10N.pm,v 1.1 2006/07/16 05:45:42 nekofar Exp $
+
+package Socializer::L10N;
+use strict;
+use base 'MT::Plugin::L10N';
+
+1;
---
+++ plugins/Socializer/lib/Socializer/L10N/fa.pm
@@ -0,0 +1,22 @@
+# Socializer plugin for Movable Type
+# Author: Milad Nekofar (http://www.nekofar.com)
+# Released under the Artistic License
+#
+# $Id: fa.pm,v 1.1 2006/07/16 05:45:42 nekofar Exp $
+
+package Socializer::L10N::fa;
+
+use strict;
+use base 'Socializer::L10N::en_us';
+use vars qw( %Lexicon );
+
+## The following is the translation table.
+
+%Lexicon = (
+    'Socializer' => 'سوسیالایزر',
+    'Add socializer link to your entries.' => 'لینک سوسیالایزر را به نوشته های شما می افزاید.',
+    'Milad Nekofar' => 'میلاد نکوفر',   
+);
+
+1;
+
